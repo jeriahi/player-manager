d8fab1af0a4a415f8dff374ef09fbfb5f0a28fe5
jalel-eddine <jaleleddine90@gmail.com>
2021-11-07 13:09:57 +0100

    Initial commit

---
+++ src/main/java/com/crud/playermanager/PlayerManagerApplication.java
@@ -0,0 +1,13 @@
+package com.crud.playermanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayerManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlayerManagerApplication.class, args);
+    }
+
+}
